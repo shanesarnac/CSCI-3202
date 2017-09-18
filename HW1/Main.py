5165ed8f3b2e569fe2b644f981539ac97e3575b3
Shane Sarnac <shane.sarnac@colorado.edu>
2017-09-18 17:20:52 -0600

    Initial Code commit

---
+++ HW1/Main.py
@@ -0,0 +1,10 @@
+from MilkStateNode import MilkStateNode
+
+def main():
+	jugA = 40
+	jugB = 40
+	jugC = 4
+	jugD = 5
+	initial_state = MilkStateNode(jugA, jugB, jugC, jugD)
+	
+main()
---
+++ HW1/MilkStateNode.py
@@ -0,0 +1,20 @@
+class MilkStateNode:
+	max_jug_a = 40
+	max_jug_b = 40
+	max_jug_c = 4
+	max_jug_d = 5
+	
+	def __init__(self, jug_a, jug_b, jug_c, juc_d):
+		self.state = {jug_a, jug_b, jug_c, juc_d}
+		
+	def setMaxJugA(self, maxA):
+		self.max_jug_a = maxA
+		
+	def setMaxJugB(self, maxB):
+		self.max_jug_b = maxB
+		
+	def setMaxJugC(self, maxC):
+		self.max_jug_c = maxC
+		
+	def setMaxJugD(self, maxD):
+		self.max_jug_d = maxD
