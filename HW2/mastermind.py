e308e3f8060aacc1920d82f0af8fd567aff05fa7
Shane Sarnac <shane.sarnac@colorado.edu>
2017-10-15 22:49:08 -0600

    defined basic Mastermind class

--- HW2/mastermind.py
+++ HW2/mastermind.py
@@ -3,4 +3,10 @@
 # October 2017
 # This code should not be used by anyone besides myself without my permission
 
+class Mastermind:
+	def __init__(self):
+		print("Hello World!")
 
+def main():
+	Mastermind()
+main()
