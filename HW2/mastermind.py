ca8077d3eff87fb99821dc6269a2e2fe1eca2f03
Shane Sarnac <shane.sarnac@colorado.edu>
2017-10-15 22:56:55 -0600

    Added classes for Guess and Color

--- HW2/mastermind.py
+++ HW2/mastermind.py
@@ -3,7 +3,25 @@
 # October 2017
 # This code should not be used by anyone besides myself without my permission
 
+MAX_COLORS = 4
+
+class Color:
+	RED = "Red"
+	BLUE = "Blue"
+	ORANGE = "Orange"
+	WHITE = "White"
+	
+class Guess:
+	colors_list = []
+	def __init__(self, lst):
+		if len(lst) != MAX_COLORS:
+			print("Invalid guess")
+			return
+		self.colors_list = lst
+		
+
 class Mastermind:
+	possible_guesses = []
 	def __init__(self):
 		print("Hello World!")
 
