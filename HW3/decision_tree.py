22c8a8f809370e205ff5d0ff9afe8936598f8d1a
Shane Sarnac <shane.sarnac@colorado.edu>
2017-11-12 21:53:49 -0700

    Initial Problem 3 setup

---
+++ HW3/decision_tree.py
@@ -0,0 +1,27 @@
+
+# TV Attributes
+# Seen ~ Seen before
+# Enj ~ Enjoyed
+# Length ~ Length of show (<=30 min)
+# Super ~ Supernatural elements
+# Survive ~ Survival Show
+# Hero ~ Super Hero
+# Reality ~ Reality Tv
+# Funny ~ Commedy 
+
+SHOW_ATTRIBUTES = {"Seen":False, "Enj":False, "Length":False, 
+					"Super":False, "Survive":False, "Hero":False, 
+					"Reality":False, "Funny":False
+					}
+
+class TV_Show:
+	
+	def __init__(self, show_name, attr):
+		self.name = show_name
+		self.attributes = attr
+
+
+def main():
+	
+	
+main()
