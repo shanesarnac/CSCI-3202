de0b8118a28b4265be567e36794f9b6740211d43
Shane Sarnac <shane.sarnac@colorado.edu>
2017-12-05 12:46:50 -0700

    Developed basic House class

--- HW4/neighborhood.py
+++ HW4/neighborhood.py
@@ -1,12 +1,22 @@
-
+# Author: Shane Sarnac
+# Date: 5 December 2017
+# This code should not be used for any purpose without my permission. 
 
 class House:
-	def __init__(self):
-		print("Hello World!")
+	occupied = 0
+	def __init__(self, occ):
+		if occ < 0:
+			print("invalid occupant")
+			exit()
+		self.occupied = occ
+		
+	def isOccupied(self):
+		return self.occupied != 0
 		
 class Neighborhood:
 	def __init__(self):
-		House()
+		
+		House(1)
 
 def main():
 	Neighborhood()
